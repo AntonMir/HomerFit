9b48117226234e7cce940b67343b5eb7b3e43c25
Р34-Антон <r34-anton@monowork.pro>
2024-05-12 16:51:43 +0300

    feat: some methods to Trainings History Service

--- src/db/services/trainings-history.service.ts
+++ src/db/services/trainings-history.service.ts
@@ -16,4 +16,28 @@ export class TrainingsHistoryService {
             logger.error('TrainingsService > createTraining > ', error);
         }
     }
+
+    /**
+     * Получить тренировку по ID
+     * @param _id
+     */
+    async getOneById(_id: string) {
+        try {
+            return TrainingHistory.findOne({ _id });
+        } catch (error) {
+            logger.error('TrainingsService > createTraining > ', error);
+        }
+    }
+
+    /**
+     * Получить историю по training ID
+     * @param trainingId
+     */
+    async getAllByTrainingId(trainingId: string) {
+        try {
+            return TrainingHistory.find({ trainingId }).exec();
+        } catch (error) {
+            logger.error('TrainingsService > createTraining > ', error);
+        }
+    }
 }
